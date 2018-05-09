d4a3b09cabba2bec9d9817b6aef43403a94d71c8
Benjamin Fair <benjaminfair@google.com>
2018-05-09 15:36:52 -0700

    arch: npcm: allow switching serial port mode
    Read the desired serial port configuration from the device tree and
    apply it if present. This allows different platforms to change how the
    serial devices inside of an NPCM SoC are connected.
    
    Signed-off-by: Benjamin Fair <benjaminfair@google.com>

--- arch/arm/mach-npcm/npcm7xx.c
+++ arch/arm/mach-npcm/npcm7xx.c
@@ -3,11 +3,15 @@
 // Copyright 2018 Google, Inc.
 
 #include <linux/kernel.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
 #include <linux/types.h>
 #include <asm/mach/arch.h>
 #include <asm/mach-types.h>
 #include <asm/mach/map.h>
 #include <asm/hardware/cache-l2x0.h>
+
+#define NPCM7XX_SPSWC_REG 0x038	/* Serial ports switch control register */
 
 static const char *const npcm7xx_dt_match[] = {
 	"nuvoton,npcm750",
@@ -15,9 +19,34 @@ static const char *const npcm7xx_dt_match[] = {
 	NULL
 };
 
+void __init npcm7xx_init_late(void)
+{
+	struct device_node *gcr_np;
+	void __iomem *gcr_base;
+	u32 spswc;
+
+	gcr_np = of_find_compatible_node(NULL, NULL, "nuvoton,npcm750-gcr");
+	if (!gcr_np) {
+		pr_err("no gcr device node\n");
+		return;
+	}
+	gcr_base = of_iomap(gcr_np, 0);
+	if (!gcr_base) {
+		pr_err("could not iomap gcr");
+		return;
+	}
+
+	/* Set serial port connectivity mode if specified */
+	if (of_property_read_u32(of_root, "nuvoton,npcm750-spswc", &spswc) == 0)
+		iowrite32(spswc, gcr_base + NPCM7XX_SPSWC_REG);
+
+	iounmap(gcr_base);
+}
+
 DT_MACHINE_START(NPCM7XX_DT, "NPCM7XX Chip family")
 	.atag_offset	= 0x100,
 	.dt_compat	= npcm7xx_dt_match,
 	.l2c_aux_val	= 0x0,
 	.l2c_aux_mask	= ~0x0,
+	.init_late	= npcm7xx_init_late,
 MACHINE_END
