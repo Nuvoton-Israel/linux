569be413e8d5b92a0651125eaecc6840635165db
yshukrun70456 <yshukron70@gmail.com>
2026-08-13 10:11:53 -0500

    Revert "modpost: add ARM relocation compatibility definitions"
    This reverts commit 6e166437b199bac69418e6cb6b17883ed0e0ce15.

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -27,17 +27,6 @@
 #include "modpost.h"
 #include "../../include/linux/license.h"
 
-#ifndef R_ARM_MOVW_ABS_NC
-#define R_ARM_MOVW_ABS_NC      43
-#define R_ARM_MOVT_ABS         44
-#define R_ARM_CALL             28
-#define R_ARM_JUMP24           29
-#define R_ARM_THM_MOVW_ABS_NC  47
-#define R_ARM_THM_MOVT_ABS     48
-#define R_ARM_THM_JUMP19       51
-#define R_ARM_THM_JUMP24       30
-#endif
-
 static bool module_enabled;
 /* Are we using CONFIG_MODVERSIONS? */
 static bool modversions;
